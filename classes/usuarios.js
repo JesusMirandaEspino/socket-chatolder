8b3f1eb4e7c3de8cec2340cb270a05df29d3af57
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-06-30 22:06:09 -0500

    persona class

---
+++ classes/usuarios.js
@@ -0,0 +1,54 @@
+class Usuarios {
+
+    constructor(){
+        this.personas = [];
+    }
+
+
+    agregarPersona( id, nombre ){
+
+        let persona = { id, nombre };
+
+        this.personas.push( persona );
+
+        return this.personas;
+    }
+
+
+
+    
+    getPesona( id ){
+        let persona = this.personas.filter( persona => {
+            return persona.id === id }
+        )[0];
+
+        return persona;
+    }
+
+
+    getPeronas(){
+        return this.personas;
+    }
+
+    getPersonasSalas(sala){
+        //...
+    }
+
+    borrarPersona(id){
+
+        let personaBorrada = this.getPesona(id)
+
+        this.personas = this.personas.filter( persona => {
+            return persona.id != id;
+        } );
+
+        return personaBorrada;
+    }
+
+}
+
+
+
+module.exports = {
+    Usuarios
+}
